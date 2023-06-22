bd4bbedc55359d4323347bde6c0db8830f58716f
Trex0319 <weichung22y@forwardschool.edu.my>
2023-06-22 13:54:34 +0800

    update

--- pages/users/manage-users-edit.php
+++ pages/users/manage-users-edit.php
@@ -40,7 +40,7 @@
 
     <div class="container mx-auto my-5" style="max-width: 700px;">
       <div class="d-flex justify-content-between align-items-center mb-2">
-        <h1 class="h1">Edit User</h1>
+        <h1 class="h1 text-white">Edit User</h1>
       </div>
       <div class="card mb-2 p-4">
       <form 
